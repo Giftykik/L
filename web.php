548e2101930fbbb0fe7326845dd17f8ab681c09a
Giftykik <87465249+Giftykik@users.noreply.github.com>
2021-07-19 09:42:23 +0300

    Add files via upload

---
+++ web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+Route::get('/', function () {
+    return view('welcome'); 
+});
+*/
+Route::get('/', 'PostController@index');
+
+
+//instead of those 7 strings below
+Route::resource('/post', 'PostController');
+
+
+/*
+
+Route::get('post/', 'PostController@index')->name('post.index');
+Route::get('post/create', 'PostController@create')->name('post.create');
+Route::post('post/', 'PostController@store')->name('post.store');
+Route::get('post/show/{id}', 'PostController@show')->name('post.show');
+Route::get('post/edit/{id}', 'PostController@edit')->name('post.edit');
+Route::patch('post/show/{id}', 'PostController@update')->name('post.update');
+Route::delete('post/{id}', 'PostController@destroy')->name('post.destroy');
+
+*/
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
